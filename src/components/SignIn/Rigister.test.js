68216a2176288d004b8b0e1d3659af3a0a64dbd8
Mrdeyi <mikeday078@gmail.com>
2022-09-23 05:46:35 +0200

    more tests

--- src/components/SignIn/Rigister.test.js
+++ src/components/SignIn/Rigister.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ReactDOM } from 'react-dom';
 import {render, cleanup, screen} from '@testing-library/react'
 import "@testing-library/jest-dom/extend-expect";
-import Register from './Register';
+import Index from '../../App';
 import userEvent from '@testing-library/user-event';
 
 afterEach(cleanup)
@@ -13,26 +13,10 @@ describe("Checking if the test file is working", () => {
     })
   })
 
-  //test if form is rendered
-  it('render form',()=>{
-    const {getByTestId}=render(<Register/>);
-    expect(screen.getByTestId('section')).toBeInTheDocument();
-    expect(screen.getByTestId('form')).toBeInTheDocument();
-  
-  })
-
-  //test if foot is rendered
-  it('render foot',()=>{
-    const {getByTestId}=render(<Register/>);
-    expect(screen.getByTestId('section')).toBeInTheDocument();
-    expect(screen.getByTestId('foot')).toBeInTheDocument();
-  
-  })
-
-  //test if icons is rendered
-  it('render icons',()=>{
-    const {getByTestId}=render(<Register/>);
-    expect(screen.getByTestId('section')).toBeInTheDocument();
-    expect(screen.getByTestId('icons')).toBeInTheDocument();
+  //test if index is rendered
+  it('render index',()=>{
+    const {getByTestId}=render(<Index/>);
+    expect(screen.getByTestId('app')).toBeInTheDocument();
+   
   
   })
